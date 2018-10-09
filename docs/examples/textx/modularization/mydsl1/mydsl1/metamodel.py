e108650177f1bcdbb3bf2e057cfd5a3001b2ff01
goto40 <pierre.bayerl@googlemail.com>
2018-10-09 21:50:19 +0200

    missing file

--- docs/examples/textx/modularization/mydsl1/mydsl1/metamodel.py
+++ docs/examples/textx/modularization/mydsl1/mydsl1/metamodel.py
@@ -6,10 +6,18 @@ import mydsl
 
 def get_metamodel():
     this_folder = dirname(abspath(__file__))
+
+    # get the "mydsl" meta model
     other_meta_model = mydsl.get_metamodel()
-    meta_model = metamodel_from_file(join(this_folder,"MyDsl1.tx"), 
+
+    # create the meta model and reference "mydsl"
+    meta_model = metamodel_from_file(
+        join(this_folder,"MyDsl1.tx"), 
         referenced_metamodels=[other_meta_model])
-    meta_model.register_scope_providers({"*.*": scoping_providers.PlainNameImportURI()})
+
+    # register scope provider (allow import models into mydsl1 models)
+    meta_model.register_scope_providers(
+        {"*.*": scoping_providers.PlainNameImportURI()})
 
     # register file endings
     scoping.MetaModelProvider.add_metamodel("*.mydsl", other_meta_model)
